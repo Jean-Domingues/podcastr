58a078f2e68061cc372e7671687b31db089e60a0
Jean Domigues <jeandomingues752@gmail.com>
2021-04-21 22:03:39 -0300

    Configurando chamadas a api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
-export default function Home() {
+export default function Home(props) {
   return (
-    <h1>Index</h1>
+    <div>
+      <h2>{JSON.stringify(props.episodes)}</h2>
+    </div>
   )
+}
+
+export async function getStaticProps(){
+  const response = await fetch('http://localHost:3333/episodes')
+  const episodes = await response.json()
+
+  return {
+    props: {
+      episodes,
+    },
+    revalidate: 900,
+  }
 }
